e05f1a75cbde200f725c1047d11289294194f830
Emersson Mendoza <emersson126@gmail.com>
2020-04-26 12:06:07 -0500

    Agregando Oculus parte 02
    Para que se conecte de forma adecuada

--- oculus.py
+++ oculus.py
@@ -1,7 +1,6 @@
 """
 osg module
 Part of osgpyplusplus python bindings for OpenSceneGraph C++ library
-https://github.com/JaneliaSciComp/osgpyplusplus
 The core osg library provides the basic scene graph classes such as Nodes, 
 State and Drawables, and maths and general helper classes. 
 """
